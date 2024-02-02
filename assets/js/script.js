c7d888d9046d28f7a8431e6bacdf8572b88531a5
haci1995 <hajiaghaaa@code.edu.az>
2024-02-03 00:14:34 +0400

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,101 @@
+"use strict"
+
+// Task 1
+
+
+// function getNum(n){
+  
+//   if(n % 3 == 0 && n % 7 == 0){
+//     console.log("This number is divide");
+//   }
+//   else{
+//     console.log("This number is not divide");
+//   }
+// }
+
+// getNum(21);
+
+// Task 2
+
+
+// function factorial(numb) {
+//     let factorial = 1;
+//     if (numb > 0) {
+//       for (let i = 1; i <= numb; i++) {
+//         factorial *= i;
+//       }
+//     } else {
+//       console.log("Number is negative");
+//     }
+//     console.log(factorial);
+//   }
+//   factorial(5)
+
+
+// Task 3
+
+
+// function sumOfSquare(numbers){
+//   let sum = 0;
+//   let square = 1;
+//   for (let i = 0; i < numbers.length; i++) {
+//     if(numbers[i] % 2 == 0){
+//       square = numbers[i] ** 2;
+//       sum += square;
+//     }
+//   }
+//   console.log(sum);
+// }
+
+// let numbers = [1,2,4,6,7,9];
+// sumOfSquare(numbers);
+
+
+
+// Task 4
+
+
+// function login(mail,password){
+// if(mail == "cavid@code.edu.az" && password == "12345"){
+//   console.log("Login is successfull!");
+// }
+// else{
+//   console.log("Mail or password is wrong!");
+// }
+// }
+
+// login("cavid@code.edu.az","12345")
+
+
+
+// Task 5
+
+
+// function sumOfOddNums(numbers){
+// let sum = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//   if(numbers[i] % 2 == 1)
+//   sum += numbers[i];
+// }
+// console.log(sum);
+// }
+
+// let numbers = [3,6,8,9];
+// sumOfOddNums(numbers);
+
+
+
+// Task 6
+
+
+// function countOfEvenNums(numbers){
+//   let count = 0;
+//   for (let i = 0; i < numbers.length; i++) {
+//     if(numbers[i] % 2 == 0)
+//     count++;
+//   }
+//   console.log(count);
+//   }
+  
+//   let numbers = [,6,8,9,14,17];
+//   countOfEvenNums(numbers);
